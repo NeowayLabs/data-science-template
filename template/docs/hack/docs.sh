bd816164d302403dc9c6d20680df204bb57533f5
Vitor Hugo <vitor.deluca@neoway.com.br>
2019-10-02 10:59:06 -0300

    Mac compatibility has just been fixed.

--- template/docs/hack/docs.sh
+++ template/docs/hack/docs.sh
@@ -11,9 +11,9 @@ rm -rf docs/build/*
 mkdir -p docs/source/md/
 cp docs/*.md docs/source/md/
 # Change file to show "API" in the table of contents rather than the package's name
-sed -i '0,/model_name/s//API/' docs/source/apidoc/modules.rst
+sed -i.bak '0,/model_name/s//API/' docs/source/apidoc/modules.rst
 # Show package's name in the home page
-sed -i '0,/project/s//model_name/' docs/source/index.rst
+sed -i.bak '0,/project/s//model_name/' docs/source/index.rst
 # Generate html docs
 make -C docs/ html
 # Clear temp files at source/md directory
